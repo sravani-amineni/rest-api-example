49cc5d6a2dbd15b757829ef5914d7fbfee7f9abf
naresh <nareshbabuy4u@gmail.com>
2025-02-12 17:08:05 +0800

    implemented deleteProduct successfully

--- src/main/java/com/rest/api/controller/RestApiExampleController.java
+++ src/main/java/com/rest/api/controller/RestApiExampleController.java
@@ -1,22 +1,58 @@
 package com.rest.api.controller;
 
 import com.rest.api.pojo.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController("/")
 public class RestApiExampleController {
+
+    HashMap<String, Product> map = new HashMap<>();
+
     @GetMapping("/getTest")
     public String getTest(@RequestParam String s) {
         System.out.println("in controller...");
-              s=s.concat("sravani");
+        s = s.concat("sravani");
 
         return s;
     }
-    @PostMapping("/product")
-    public String product(@RequestBody Product p) {
-        // In a real-world application, you would save the user to the database
-        return "Product" + p.getColour()+ " with size " + p.getSize() + " created successfully!";
+
+    @PostMapping("/createProduct")
+    public String createProduct(@RequestBody Product p) {
+
+
+        // Adding key-value pairs
+        map.put(p.getProductId(), p);
+
+
+        return "Product" + p.getColour() + " with size " + p.getSize() + " created successfully!";
     }
 
+    @GetMapping("/getProduct")
+    public Product getProduct(@RequestParam String id) {
+        Product product = map.get(id);
+        return product;
 
+    }
+    @GetMapping("/getAllProduct")
+    public Collection<Product> getAllProduct()
+    {
+        return map.values();
+    }
+
+    @DeleteMapping("/deleteProduct")
+    public String deleteProduct(@RequestParam String id) {
+        if (map.containsKey(id)) {
+            Product p = map.remove(id);
+            return "Product Id " + p.getProductId() + " deleted successfully.";
+        } else {
+            return "Product Id is Not available" ;
+        }
+    }
 }
+
--- src/main/java/com/rest/api/pojo/Product.java
+++ src/main/java/com/rest/api/pojo/Product.java
@@ -3,6 +3,20 @@ package com.rest.api.pojo;
 public class Product {
     private String colour;
     private char size;
+    public  String productId;
+    public String productName;
+    public String getProductId(){
+        return productId;
+    }
+    public void setProductId(String productId){
+        this.productId = productId;
+    }
+       public String getProductName(){
+        return productName;
+       }
+       public void setProductName(String productName){
+        this.productName= productName;
+       }
 
     public String getColour() {
         return colour;
@@ -19,4 +33,5 @@ public class Product {
     public void setSize(char size) {
         this.size = size;
     }
+
 }
